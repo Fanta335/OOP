4d7b3136923b2a0e657977704f4ed9d351d580e5
Fanta335 <fantayusuke5210@gmail.com>
2021-08-21 17:30:33 +0900

    add instances and move AbstructListInteger classes.

--- chapter-3/src/AbstractListInteger.ts
+++ chapter-3/src/AbstractListInteger.ts
@@ -1,129 +1,129 @@
-abstract class AbstractListInteger {
-  protected _initialList: number[];
+// abstract class AbstractListInteger {
+//   protected _initialList: number[];
 
-  constructor();
-  constructor(arr: number[]);
-  constructor(arr?: number[]) {
-    if (arr === undefined) this._initialList = [];
-    else this._initialList = arr;
-  }
+//   constructor();
+//   constructor(arr: number[]);
+//   constructor(arr?: number[]) {
+//     if (arr === undefined) this._initialList = [];
+//     else this._initialList = arr;
+//   }
 
-  public get originalList() {
-    return this._initialList;
-  }
+//   public get originalList() {
+//     return this._initialList;
+//   }
 
-  public set originalList(elements: number[]) {
-    this._initialList = elements;
-  }
+//   public set originalList(elements: number[]) {
+//     this._initialList = elements;
+//   }
 
-  // AbstractListIntegerが実装しなければならないメソッド。
-  public abstract get(position: number): number;
-  public abstract add(element: number): void; // リストの最後に追加します。
-  public abstract add(elements: number[]): void; // リストの最後の要素に追加します。
-  public abstract pop(): number | undefined; // リストの最後から削除します。削除した要素を返します。
-  public abstract addAt(position: number, element: number): void; // 指定された位置に要素を追加します。
-  public abstract addAt(position: number, elements: number[]): void; // 指定された位置に複数の要素を追加します。
-  public abstract removeAt(position: number): number; // 指定した位置にある要素を削除します。削除した要素を返します。
-  public abstract removeAllAt(start: number): void; // 指定された位置から始まるすべての要素を削除します。
-  public abstract removeAllAt(start: number, end: number): void; // startからendまでの全ての要素を削除します。
-  public abstract subList(start: number): AbstractListInteger; // AbstractListIntegerの部分リストを、指定された位置から最後まで返します。
-  public abstract subList(start: number, end: number): AbstractListInteger; // startからendまでのAbstractListIntegerの部分リストを返します。
-}
+//   // AbstractListIntegerが実装しなければならないメソッド。
+//   public abstract get(position: number): number;
+//   public abstract add(element: number): void; // リストの最後に追加します。
+//   public abstract add(elements: number[]): void; // リストの最後の要素に追加します。
+//   public abstract pop(): number | undefined; // リストの最後から削除します。削除した要素を返します。
+//   public abstract addAt(position: number, element: number): void; // 指定された位置に要素を追加します。
+//   public abstract addAt(position: number, elements: number[]): void; // 指定された位置に複数の要素を追加します。
+//   public abstract removeAt(position: number): number; // 指定した位置にある要素を削除します。削除した要素を返します。
+//   public abstract removeAllAt(start: number): void; // 指定された位置から始まるすべての要素を削除します。
+//   public abstract removeAllAt(start: number, end: number): void; // startからendまでの全ての要素を削除します。
+//   public abstract subList(start: number): AbstractListInteger; // AbstractListIntegerの部分リストを、指定された位置から最後まで返します。
+//   public abstract subList(start: number, end: number): AbstractListInteger; // startからendまでのAbstractListIntegerの部分リストを返します。
+// }
 
-class IntegerArrayList extends AbstractListInteger {
-  constructor();
-  constructor(arr: number[]);
-  constructor(arr?: number[]) {
-    if (arr === undefined) super();
-    else super(arr);
-  }
+// class IntegerArrayList extends AbstractListInteger {
+//   constructor();
+//   constructor(arr: number[]);
+//   constructor(arr?: number[]) {
+//     if (arr === undefined) super();
+//     else super(arr);
+//   }
 
-  public get originalList() {
-    return super.originalList;
-  }
+//   public get originalList() {
+//     return super.originalList;
+//   }
 
-  public set originalList(elements: number[]) {
-    super.originalList = elements;
-  }
+//   public set originalList(elements: number[]) {
+//     super.originalList = elements;
+//   }
 
-  public get(position: number) {
-    return this.originalList[position];
-  }
+//   public get(position: number) {
+//     return this.originalList[position];
+//   }
 
-  public add(element: number): void;
-  public add(elements: number[]): void;
-  public add(elementOrElements: number | number[]): void {
-    if (typeof elementOrElements === "number") {
-      let arr = this.originalList;
-      arr.push(elementOrElements);
-      this.originalList = arr;
-    } else this.originalList.concat(elementOrElements);
-  }
+//   public add(element: number): void;
+//   public add(elements: number[]): void;
+//   public add(elementOrElements: number | number[]): void {
+//     if (typeof elementOrElements === "number") {
+//       let arr = this.originalList;
+//       arr.push(elementOrElements);
+//       this.originalList = arr;
+//     } else this.originalList.concat(elementOrElements);
+//   }
 
-  public pop(): number | undefined {
-    let ele = this.originalList.pop();
-    if (ele !== undefined) return ele;
-    return undefined;
-  }
+//   public pop(): number | undefined {
+//     let ele = this.originalList.pop();
+//     if (ele !== undefined) return ele;
+//     return undefined;
+//   }
 
-  public addAt(position: number, element: number): void;
-  public addAt(position: number, elements: number[]): void;
-  public addAt(position: number, elementOrElements: number | number[]): void {
-    let left = this.originalList.slice(0, position);
-    let right = this.originalList.slice(position);
-    if (typeof elementOrElements === "number") {
-      left.push(elementOrElements);
-      this.originalList = left.concat(right);
-    } else {
-      this.originalList = left.concat(elementOrElements, right);
-    }
-  }
+//   public addAt(position: number, element: number): void;
+//   public addAt(position: number, elements: number[]): void;
+//   public addAt(position: number, elementOrElements: number | number[]): void {
+//     let left = this.originalList.slice(0, position);
+//     let right = this.originalList.slice(position);
+//     if (typeof elementOrElements === "number") {
+//       left.push(elementOrElements);
+//       this.originalList = left.concat(right);
+//     } else {
+//       this.originalList = left.concat(elementOrElements, right);
+//     }
+//   }
 
-  public removeAt(position: number): number {
-    // 指定した位置にある要素を削除します。削除した要素を返します。
-    if (position >= this.originalList.length) {
-      console.log("Invalid position. Position is bigger than ths list length.");
-      return -1;
-    }
-    let left = this.originalList.slice(0, position);
-    let right = this.originalList.slice(position + 1);
-    let res = this.originalList[position];
-    this.originalList = left.concat(right);
-    return res;
-  }
+//   public removeAt(position: number): number {
+//     // 指定した位置にある要素を削除します。削除した要素を返します。
+//     if (position >= this.originalList.length) {
+//       console.log("Invalid position. Position is bigger than ths list length.");
+//       return -1;
+//     }
+//     let left = this.originalList.slice(0, position);
+//     let right = this.originalList.slice(position + 1);
+//     let res = this.originalList[position];
+//     this.originalList = left.concat(right);
+//     return res;
+//   }
 
-  public removeAllAt(start: number): void; // 指定された位置から始まるすべての要素を削除します。
-  public removeAllAt(start: number, end: number): void; // startからendまでの全ての要素を削除します。
-  public removeAllAt(start: number, end?: number): void {
-    if (start >= this.originalList.length || start < 0) {
-      console.log("Invalid start index.");
-      return;
-    } else if (end !== undefined && end >= this.originalList.length) {
-      console.log("Invalid end index.");
-      return;
-    }
+//   public removeAllAt(start: number): void; // 指定された位置から始まるすべての要素を削除します。
+//   public removeAllAt(start: number, end: number): void; // startからendまでの全ての要素を削除します。
+//   public removeAllAt(start: number, end?: number): void {
+//     if (start >= this.originalList.length || start < 0) {
+//       console.log("Invalid start index.");
+//       return;
+//     } else if (end !== undefined && end >= this.originalList.length) {
+//       console.log("Invalid end index.");
+//       return;
+//     }
 
-    let left = this.originalList.slice(0, start);
-    if (typeof end === "number") {
-      let right = this.originalList.slice(end);
-      this.originalList = left.concat(right);
-    } else {
-      this.originalList = left;
-    }
-  }
+//     let left = this.originalList.slice(0, start);
+//     if (typeof end === "number") {
+//       let right = this.originalList.slice(end);
+//       this.originalList = left.concat(right);
+//     } else {
+//       this.originalList = left;
+//     }
+//   }
 
-  public subList(start: number): IntegerArrayList; // AbstractListIntegerの部分リストを、指定された位置から最後まで返します。
-  public subList(start: number, end: number): IntegerArrayList; // startからendまでのAbstractListIntegerの部分リストを返します。
-  public subList(start: number, end?: number): IntegerArrayList {
-    let array = [];
-    if (typeof end === "number") {
-      array = this.originalList.slice(start, end);
-    } else {
-      array = this.originalList.slice(start);
-    }
-    return new IntegerArrayList(array);
-  }
-}
+//   public subList(start: number): IntegerArrayList; // AbstractListIntegerの部分リストを、指定された位置から最後まで返します。
+//   public subList(start: number, end: number): IntegerArrayList; // startからendまでのAbstractListIntegerの部分リストを返します。
+//   public subList(start: number, end?: number): IntegerArrayList {
+//     let array = [];
+//     if (typeof end === "number") {
+//       array = this.originalList.slice(start, end);
+//     } else {
+//       array = this.originalList.slice(start);
+//     }
+//     return new IntegerArrayList(array);
+//   }
+// }
 
 // class IntegerLinkedList extends AbstractListInteger{
 //   private
--- chapter-3/src/DataStructureInterface.ts
+++ chapter-3/src/DataStructureInterface.ts
@@ -25,6 +25,69 @@ class NodeInt {
   }
 }
 
+class MyStack implements StackInt {
+  private _head: NodeInt | undefined;
+
+  constructor() {
+    this._head = undefined;
+  }
+
+  push(data: number): void {
+    let node = new NodeInt(data);
+    node.next = this._head;
+    this._head = node;
+  }
+
+  pop(): number | undefined {
+    if (this._head === undefined) return undefined;
+
+    let temp = this._head;
+    this._head = this._head.next;
+    return temp.data;
+  }
+
+  peekLast(): number | undefined {
+    if (this._head === undefined) return undefined;
+    return this._head.data;
+  }
+}
+
+class MyQueue implements QueueInt {
+  private _head: NodeInt | undefined;
+  private _tail: NodeInt | undefined;
+
+  constructor() {
+    this._head = undefined;
+    this._tail = undefined;
+  }
+
+  // 末尾に要素を追加する
+  push(data: number): void {
+    if (this._head === undefined) {
+      this._head = new NodeInt(data);
+    } else if (this._tail === undefined) {
+      this._tail = new NodeInt(data);
+      this._head.next = this._tail;
+    } else {
+      this._tail.next = new NodeInt(data);
+      this._tail = this._tail.next;
+    }
+  }
+
+  // 先頭から要素を削除し、削除した要素を返す
+  poll(): number | undefined {
+    if (this._head === undefined) return undefined;
+    let temp = this._head;
+
+    if (this._head.next === undefined) {
+      this._head === undefined;
+      this._tail === undefined;
+    } else this._head = this._head.next;
+
+    return temp.data;
+  }
+}
+
 class MyDequeInt implements DequeInt {
   private _head: NodeInt | undefined;
   private _tail: NodeInt | undefined;
@@ -95,6 +158,133 @@ class MyDequeInt implements DequeInt {
   }
 }
 
+abstract class AbstractListInteger {
+  protected _initialList: number[];
+
+  constructor();
+  constructor(arr: number[]);
+  constructor(arr?: number[]) {
+    if (arr === undefined) this._initialList = [];
+    else this._initialList = arr;
+  }
+
+  public get originalList() {
+    return this._initialList;
+  }
+
+  public set originalList(elements: number[]) {
+    this._initialList = elements;
+  }
+
+  // AbstractListIntegerが実装しなければならないメソッド。
+  public abstract get(position: number): number;
+  public abstract add(element: number): void; // リストの最後に追加します。
+  public abstract add(elements: number[]): void; // リストの最後の要素に追加します。
+  public abstract pop(): number | undefined; // リストの最後から削除します。削除した要素を返します。
+  public abstract addAt(position: number, element: number): void; // 指定された位置に要素を追加します。
+  public abstract addAt(position: number, elements: number[]): void; // 指定された位置に複数の要素を追加します。
+  public abstract removeAt(position: number): number; // 指定した位置にある要素を削除します。削除した要素を返します。
+  public abstract removeAllAt(start: number): void; // 指定された位置から始まるすべての要素を削除します。
+  public abstract removeAllAt(start: number, end: number): void; // startからendまでの全ての要素を削除します。
+  public abstract subList(start: number): AbstractListInteger; // AbstractListIntegerの部分リストを、指定された位置から最後まで返します。
+  public abstract subList(start: number, end: number): AbstractListInteger; // startからendまでのAbstractListIntegerの部分リストを返します。
+}
+
+class IntegerArrayList extends AbstractListInteger {
+  constructor();
+  constructor(arr: number[]);
+  constructor(arr?: number[]) {
+    if (arr === undefined) super();
+    else super(arr);
+  }
+
+  public get originalList() {
+    return super.originalList;
+  }
+
+  public set originalList(elements: number[]) {
+    super.originalList = elements;
+  }
+
+  public get(position: number) {
+    return this.originalList[position];
+  }
+
+  public add(element: number): void;
+  public add(elements: number[]): void;
+  public add(elementOrElements: number | number[]): void {
+    if (typeof elementOrElements === "number") {
+      let arr = this.originalList;
+      arr.push(elementOrElements);
+      this.originalList = arr;
+    } else this.originalList.concat(elementOrElements);
+  }
+
+  public pop(): number | undefined {
+    let ele = this.originalList.pop();
+    if (ele !== undefined) return ele;
+    return undefined;
+  }
+
+  public addAt(position: number, element: number): void;
+  public addAt(position: number, elements: number[]): void;
+  public addAt(position: number, elementOrElements: number | number[]): void {
+    let left = this.originalList.slice(0, position);
+    let right = this.originalList.slice(position);
+    if (typeof elementOrElements === "number") {
+      left.push(elementOrElements);
+      this.originalList = left.concat(right);
+    } else {
+      this.originalList = left.concat(elementOrElements, right);
+    }
+  }
+
+  public removeAt(position: number): number {
+    // 指定した位置にある要素を削除します。削除した要素を返します。
+    if (position >= this.originalList.length) {
+      console.log("Invalid position. Position is bigger than ths list length.");
+      return -1;
+    }
+    let left = this.originalList.slice(0, position);
+    let right = this.originalList.slice(position + 1);
+    let res = this.originalList[position];
+    this.originalList = left.concat(right);
+    return res;
+  }
+
+  public removeAllAt(start: number): void; // 指定された位置から始まるすべての要素を削除します。
+  public removeAllAt(start: number, end: number): void; // startからendまでの全ての要素を削除します。
+  public removeAllAt(start: number, end?: number): void {
+    if (start >= this.originalList.length || start < 0) {
+      console.log("Invalid start index.");
+      return;
+    } else if (end !== undefined && end >= this.originalList.length) {
+      console.log("Invalid end index.");
+      return;
+    }
+
+    let left = this.originalList.slice(0, start);
+    if (typeof end === "number") {
+      let right = this.originalList.slice(end);
+      this.originalList = left.concat(right);
+    } else {
+      this.originalList = left;
+    }
+  }
+
+  public subList(start: number): IntegerArrayList; // AbstractListIntegerの部分リストを、指定された位置から最後まで返します。
+  public subList(start: number, end: number): IntegerArrayList; // startからendまでのAbstractListIntegerの部分リストを返します。
+  public subList(start: number, end?: number): IntegerArrayList {
+    let array = [];
+    if (typeof end === "number") {
+      array = this.originalList.slice(start, end);
+    } else {
+      array = this.originalList.slice(start);
+    }
+    return new IntegerArrayList(array);
+  }
+}
+
 function createDeque(arr: number[]): MyDequeInt {
   let deque = new MyDequeInt();
   deque.push(arr[0]);
@@ -106,9 +296,8 @@ function createDeque(arr: number[]): MyDequeInt {
   return deque;
 }
 
-let deque = createDeque([1,2,3,4,5,6,7,8,9,10]);
+let deque = createDeque([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 console.log(deque.peekFirst());
 console.log(deque.peekLast());
 console.log(deque.pop());
 console.log(deque.peekLast());
-
